a3de4ae5be11c0289542c504838273d240d3aeb1
inpulzasolution <40728762-inpulzasolution@users.noreply.replit.com>
2025-08-22 13:46:31 +0000

    Restored to 'ad6c10dfe3f4504290b6f1034cd3e6da065dee1d'
    Replit-Restored-To: ad6c10dfe3f4504290b6f1034cd3e6da065dee1d

--- client/src/components/LocationInsuranceLogos.tsx
+++ client/src/components/LocationInsuranceLogos.tsx
@@ -84,9 +84,9 @@ const LocationInsuranceLogos = () => {
         <div className="bg-white rounded-2xl sm:rounded-3xl p-6 sm:p-8 lg:p-10 border border-gray-100">
           
           {/* Mobile Layout - Auto Slider */}
-          <div className="block md:hidden relative h-48">
-            <div className="absolute inset-0 flex justify-center items-center">
-              <div className="relative w-48 h-32">
+          <div className="block md:hidden relative">
+            <div className="w-full h-48 flex justify-center items-center">
+              <div className="relative w-72 h-48">
                 {insuranceLogos.map((logo, index) => (
                   <div 
                     key={index}
@@ -100,15 +100,15 @@ const LocationInsuranceLogos = () => {
                     <OptimizedImage
                       src={logo.src}
                       alt={logo.alt}
-                      className="w-48 h-32 object-contain filter grayscale"
+                      className="w-72 h-48 object-contain filter grayscale"
                       width={480}
                       height={336}
                       priority={index < 6}
-                      sizes="192px"
+                      sizes="288px"
                       style={{
                         aspectRatio: '10/7',
-                        minWidth: '192px',
-                        minHeight: '128px',
+                        minWidth: '288px',
+                        minHeight: '192px',
                         containIntrinsicSize: '480px 336px',
                         contentVisibility: index < 6 ? 'visible' : 'auto',
                         willChange: 'auto'
@@ -120,7 +120,7 @@ const LocationInsuranceLogos = () => {
             </div>
             
             {/* Progress dots - positioned at bottom edge of mobile container */}
-            <div className="absolute bottom-6 left-1/2 transform -translate-x-1/2 flex gap-1">
+            <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex gap-1">
               {insuranceLogos.map((_, index) => (
                 <div
                   key={index}
